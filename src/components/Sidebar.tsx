14590b3f151ce2a637c2d561a1c3c6553fb47c94
alex-willow <servicepioner@gmail.com>
2025-12-12 15:06:23 +0300

    Add tempates

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,10 +18,7 @@ export default function Sidebar({
   onSectionChange,
   isMobile,
   isMenuOpen,
-  onMenuClose,
-  isAuthenticated = false,
-  onLogout,
-  onSignInOpen,
+  onMenuClose
 }: SidebarProps) {
   const navigate = useNavigate();
   const location = useLocation();
